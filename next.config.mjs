a9b282906688fae1dd33c5f0844162ad9da08af2
Miguel Alcalde <fmiguel05@gmail.com>
2024-09-19 00:14:18 +0200

    change db

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    serverActions: true,
+  env: {
+    POSTGRES_PRISMA_URL: process.env.POSTGRES_PRISMA_URL,
+    POSTGRES_URL_NON_POOLING: process.env.POSTGRES_URL_NON_POOLING,
   },
 };
 
